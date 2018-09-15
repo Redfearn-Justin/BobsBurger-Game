4bf5fb5efbe529b972cd9199cbb929076c8db92f
Napoleon <wanderofrazgriz@gmail.com>
2018-09-15 15:33:05 -0700

    Updated DB and Connection js

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+-- Creating Database -- 
+CREATE DATABASE burgers_db;
+
+-- selecting DB to USE it --
+USE burgers_db;
+
+-- Creating table to be used to store data -- 
+CREATE TABLE burgers_table (
+id INTEGER NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers_table (burger_name)
+VALUES ('New Bacon-ings'), ('Foot Feta-ish Burger'), ('Chorizo Your Own Adventure Burger')
