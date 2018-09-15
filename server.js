4f38f4582c1999f454443d6e091c31dea5306147
Napoleon <wanderofrazgriz@gmail.com>
2018-09-15 13:54:09 -0700

    Finished Server

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 //Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var app = express();
 
 //Setting up the PORT
 var PORT = process.env.PORT || 8080;
 
+var app = express();
+
 //Serve static content for application
 app.use(express.static("public"));
 
-//Body parser set up
+//Body parser set up for URL and JSON
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
@@ -20,3 +21,11 @@ app.engine("handlebars", handlebars({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 //Importing routes
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+//Application listeners
+app.listen(PORT, function(){
+    console.log("Server is active on PORT: " + PORT);
+});
